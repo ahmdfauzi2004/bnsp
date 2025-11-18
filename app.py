195e6fae0e719c8db3a847f11655acacb34f5427
ahmdfauzi2004 <152979440+ahmdfauzi2004@users.noreply.github.com>
2025-11-18 23:02:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'hello saya sudah berhasil'
+    return '<h1>hello saya sudah berhasil update commit</h1>'
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
